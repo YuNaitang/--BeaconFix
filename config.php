7ca062d2118177996ccf52efb8b79f0a42868d30
YuNaitang <3493995759@qq.com>
2026-05-22 23:17:25 +0800

    第一版

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'db_host' => '127.0.0.1',
+  'db_port' => '3306',
+  'db_name' => 'www_yunaitang_to',
+  'db_user' => 'www_yunaitang_to',
+  'db_pass' => '7BMT63Y26wxCWKrr',
+  'site_url' => 'https://www.yunaitang.top',
+  'timezone' => 'Asia/Shanghai',
+);
